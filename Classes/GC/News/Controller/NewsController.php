5a90952029cbafdef5d33f13acf3434efd76da07
Grégory Copin <gcopin@inouit.com>
2015-01-09 14:25:04 +0100

    [add] plugin init

---
+++ Classes/GC/News/Controller/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+namespace GC\News\Controller;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Mvc\Controller\ActionController;
+
+class NewsController extends ActionController {
+
+  /**
+   * Display list of news
+   * @return string
+   */
+  public function listAction(){
+
+  }
+
+}
